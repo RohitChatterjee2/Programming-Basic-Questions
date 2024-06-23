8b42f3a19dba0c94cb8b9b8dae8d6118ca9dfcef
Rohit Chatterjee <rohitchatterjee758@gmail.com>
2024-06-24 00:00:53 +0530

    Update p1.cpp
    I added some spaces and comment lines

--- p1.cpp
+++ p1.cpp
@@ -9,9 +9,15 @@ using namespace std;
 int main() {
     /* Reading input from STDIN. Printing output to STDOUT */ 
     string str1,str2,str3;
-    cin>>str1>>str2;
+    cin>>str1>>str2; 
+
+    // Printing
     cout<<str1.size()<<" "<<str2.size()<<endl;
-    str3=str1+str2;  //Concatenating the user inputted strings
+    
+    //Concatenating the user inputted strings 
+    str3=str1+str2;  
+
+    // Printing 
     cout<<str3<<endl;
     swap(str1[0],str2[0]);  /*I am using swap function from algorithm header 
                              but you can also use a third variable*/
